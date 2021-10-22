e589a615764fa9dd8ad4f642074340f17db16ebf
Rafael Varago <rafael.varago@gmail.com>
2021-10-22 16:33:56 +0200

    Add basic regression tests for the codec

--- src/codec.rs
+++ src/codec.rs
@@ -1,15 +1,17 @@
+//! Codec for the wire protocol through which requests/responses are exchanged.
+
 use anyhow::{bail, Result};
 use bytes::BytesMut;
 use tokio_util::codec::{Decoder, Encoder};
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 pub struct LineQueryCodec {
     next_index: usize,
 }
 
 impl LineQueryCodec {
     pub fn new() -> Self {
-        Self { next_index: 0 }
+        Self::default()
     }
 }
 
@@ -49,7 +51,7 @@ impl Encoder<Response> for LineQueryCodec {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Request {
     Get { key: String },
     Set { key: String, value: String },
@@ -64,7 +66,7 @@ impl Request {
         match components[0] {
             "GET" => {
                 if components.len() != 2 {
-                    bail!("GET expects 1 argument separated by empty space")
+                    bail!("GET expects 1 argument separated by whitespace")
                 } else {
                     Ok(Request::Get {
                         key: components[1].to_owned(),
@@ -73,7 +75,7 @@ impl Request {
             }
             "SET" => {
                 if components.len() != 3 {
-                    bail!("SET expects 2 arguments separated by empty space")
+                    bail!("SET expects 2 arguments separated by whitespace")
                 } else {
                     Ok(Request::Set {
                         key: components[1].to_owned(),
@@ -136,5 +138,145 @@ impl Status {
             Status::Okay => "OKAY",
             Status::Fail => "FAIL",
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn succeeds_to_encode_status() {
+        let cases = vec![(Status::Okay, "OKAY"), (Status::Fail, "FAIL")];
+
+        cases
+            .into_iter()
+            .for_each(|(status, expected_encoded_status)| {
+                // Pre-condition.
+                // Action.
+                let encoded_status = status.encode();
+                // Post-condition.
+                assert_eq!(encoded_status, expected_encoded_status);
+            });
+    }
+
+    proptest! {
+        #[test]
+        fn fails_to_decode_request_with_invalid_command(command in invalid_request_command()) {
+            // Pre-condition.
+            let mut message = BytesMut::from(format!("{}\n", command).as_str());
+            let mut decoder = LineQueryCodec::default();
+
+            // Action.
+            let request = decoder.decode(&mut message);
+
+            // Post-condition.
+            assert!(request.is_err());
+            assert!(message.is_empty());
+        }
+    }
+
+    #[test]
+    fn fails_to_decodes_malformed_request() {
+        let cases = vec![
+            (b"GET\n".as_ref(), "get without key"),
+            (b"SET\n".as_ref(), "set without key"),
+            (b"SET key\n".as_ref(), "set without value"),
+        ];
+
+        cases.into_iter().for_each(|(message, reason)| {
+            // Pre-condition.
+            let mut decoder = LineQueryCodec::default();
+            let mut message = BytesMut::from(message);
+
+            // Action.
+            let request = decoder.decode(&mut message);
+
+            // Post-condition.
+            assert!(request.is_err(), "{}", reason);
+            assert!(message.is_empty(), "{}", reason);
+        });
+    }
+
+    #[test]
+    fn succeeds_to_decode_wellformed_request() {
+        let cases = vec![
+            (
+                b"GET key\n".as_ref(),
+                Request::Get { key: "key".into() },
+                "get key",
+            ),
+            (
+                b"SET key value\n".as_ref(),
+                Request::Set {
+                    key: "key".into(),
+                    value: "value".into(),
+                },
+                "set key to value",
+            ),
+        ];
+
+        cases
+            .into_iter()
+            .for_each(|(message, expected_request, reason)| {
+                // Pre-condition.
+                let mut decoder = LineQueryCodec::default();
+                let mut message = BytesMut::from(message);
+
+                // Action.
+                let request = decoder.decode(&mut message).unwrap();
+
+                // Post-condition.
+                assert_eq!(request, Some(expected_request), "{}", reason);
+                assert!(message.is_empty(), "{}", reason);
+            });
+    }
+
+    #[test]
+    fn succeeds_to_encode_response() {
+        let cases = vec![
+            (
+                Response::Get {
+                    key: "key".into(),
+                    value: None,
+                },
+                b"FAIL key\n".as_ref(),
+                "get without value",
+            ),
+            (
+                Response::Get {
+                    key: "key".into(),
+                    value: Some("value".into()),
+                },
+                b"OKAY key value\n".as_ref(),
+                "get with value",
+            ),
+            (
+                Response::Set { key: "key".into() },
+                b"OKAY key\n".as_ref(),
+                "set key",
+            ),
+        ];
+
+        cases
+            .into_iter()
+            .for_each(|(response, expected_message, reason)| {
+                // Pre-condition.
+                let mut encoder = LineQueryCodec::default();
+                let mut message = BytesMut::default();
+
+                // Action.
+                encoder.encode(response, &mut message).unwrap();
+
+                // Post-condition.
+                assert_eq!(message, expected_message, "{}", reason)
+            });
+    }
+
+    fn invalid_request_command() -> impl Strategy<Value = String> {
+        any::<String>().prop_filter("valid command", |cmd| {
+            !vec!["GET", "SET"].contains(&cmd.as_str())
+        })
     }
 }
